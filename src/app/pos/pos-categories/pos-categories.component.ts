ab39ab59e2ca51f976a9727e947e38b0c945d32d
LennyDennis <lennydennis@gmail.com>
2021-06-08 17:52:46 +0300

    Display pos categories card from the get endpoint

--- src/app/pos/pos-categories/pos-categories.component.ts
+++ src/app/pos/pos-categories/pos-categories.component.ts
@@ -1,28 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/services/category-service/category.service';
 import { Category } from '../../models/category';
-
-const CATEGORY_DATA: Category[] = [
-  {categoryId: 1, categoryName: 'Hydrogen 1', totalProducts: 100, status:"Active"},
-  {categoryId: 2, categoryName: 'Hydrogen 2', totalProducts: 100, status:"Active"},
-  {categoryId: 3, categoryName: 'Hydrogen 3', totalProducts: 100, status:"Active"},
-  {categoryId: 4, categoryName: 'Hydrogen 4', totalProducts: 100, status:"Active"},
-  {categoryId: 5, categoryName: 'Hydrogen 5', totalProducts: 100, status:"Active"},
-  {categoryId: 6, categoryName: 'Hydrogen 6', totalProducts: 100, status:"Active"},
-  {categoryId: 7, categoryName: 'Hydrogen 7', totalProducts: 100, status:"Active"},
-  {categoryId: 8, categoryName: 'Hydrogen 8', totalProducts: 100, status:"Active"},
-  {categoryId: 9, categoryName: 'Hydrogen 9', totalProducts: 100, status:"Active"},
-  {categoryId: 10, categoryName: 'Hydrogen 10', totalProducts: 100, status:"Active"},
-  {categoryId: 1, categoryName: 'Hydrogen 1', totalProducts: 100, status:"Active"},
-  {categoryId: 2, categoryName: 'Hydrogen 2', totalProducts: 100, status:"Active"},
-  {categoryId: 3, categoryName: 'Hydrogen 3', totalProducts: 100, status:"Active"},
-  {categoryId: 4, categoryName: 'Hydrogen 4', totalProducts: 100, status:"Active"},
-  {categoryId: 5, categoryName: 'Hydrogen 5', totalProducts: 100, status:"Active"},
-  {categoryId: 6, categoryName: 'Hydrogen 6', totalProducts: 100, status:"Active"},
-  {categoryId: 7, categoryName: 'Hydrogen 7', totalProducts: 100, status:"Active"},
-  {categoryId: 8, categoryName: 'Hydrogen 8', totalProducts: 100, status:"Active"},
-  {categoryId: 9, categoryName: 'Hydrogen 9', totalProducts: 100, status:"Active"},
-  {categoryId: 10, categoryName: 'Hydrogen 10', totalProducts: 100, status:"Active"}
-];
 
 @Component({
   selector: 'app-pos-categories',
@@ -31,12 +9,26 @@ const CATEGORY_DATA: Category[] = [
 })
 export class PosCategoriesComponent implements OnInit {
 
-  categoriesArray = CATEGORY_DATA;
+  categories = [];
 
-  constructor() { }
+  constructor(
+    private _categoryService: CategoryService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getAllCategories();
   }
+
+  getAllCategories() {
+    this._categoryService.getAllCategories().subscribe(
+      (res) => {
+        this.categories = (<any>res).categories;
+        console.log(this.categories)
+      },
+      (err) => {}
+    );
+  }
+
 
   showDiv = {
     previous : false,
